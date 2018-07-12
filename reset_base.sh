368ec372ddbf49bc115c35b06febed338f40c02e
Antonio Glavocevic <tony@it-tony-mac.local>
2018-07-12 20:16:38 +1000

    setup and reset base script

---
+++ reset_base.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+rm -rf /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer
+cp -r ./MyDediServer /home/ubuntu/.klei/DoNotStarveTogether
---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp -r ./server_dst/* /home/ubuntu/Steam/steamapps/server_dst
+sh ./reset_base.sh
