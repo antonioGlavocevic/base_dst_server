9bd0f4dabf482d4075ab7836af959925d50b7746
Antonio Glavocevic <tony@it-tony-mac.local>
2018-10-26 15:53:58 +1100

    stop cleanup

--- server_dst/bin/stop.sh
+++ server_dst/bin/stop.sh
@@ -2,9 +2,18 @@
 # Stop Master and Caves
 sh /home/ubuntu/Steam/steamapps/server_dst/bin/master_stop.sh
 sh /home/ubuntu/Steam/steamapps/server_dst/bin/cave_stop.sh
-sleep 5
+while screen -ls | grep -qE 'dst_server1|dst_server2'
+do
+  sleep 1
+done
 DATE=`date '+%Y-%m-%d_%H-%M-%S'`
-mkdir /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/logs
-mkdir /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/logs
-cp /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/logs/server_log_${DATE}.txt
-cp /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/server_log.txt /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/logs/server_log_${DATE}.txt
+mkdir -p /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/logs
+mkdir -p /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/logs
+if [ -e /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt ]
+then
+  mv /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/server_log.txt /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Master/logs/server_log_${DATE}.txt
+fi
+if [ -e /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/server_log.txt ]
+then
+  mv /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/server_log.txt /home/ubuntu/.klei/DoNotStarveTogether/MyDediServer/Caves/logs/server_log_${DATE}.txt
+fi
